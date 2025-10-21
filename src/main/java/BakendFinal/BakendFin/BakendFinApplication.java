da725aa645e576145daa6011195e291ea9bf492b
Enzo <enzocercola7@gmail.com>
2025-10-21 16:20:35 -0300

    Primer commit para guardar los archivos

---
+++ src/main/java/BakendFinal/BakendFin/BakendFinApplication.java
@@ -0,0 +1,13 @@
+package BakendFinal.BakendFin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakendFinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakendFinApplication.class, args);
+	}
+
+}
