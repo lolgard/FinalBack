274544d040e9d4012d7849cc86e31540c90a88f0
3zequiel3 <3zegonzalez@gmail.com>
2025-10-24 13:02:15 +0000

    Cambios hasta el 24/10

--- src/main/java/BakendFinal/controllers/ClienteController.java
+++ src/main/java/BakendFinal/controllers/ClienteController.java
@@ -1,6 +1,7 @@
 package BakendFinal.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +11,6 @@ import BakendFinal.entities.DTOs.client.ClienteDTO;
 import BakendFinal.entities.DTOs.client.ClienteEdit;
 import BakendFinal.entities.DTOs.client.ClientePass;
 import BakendFinal.services.client.ClienteService;
-
-import org.springframework.web.bind.annotation.PostMapping;
 
 
 @RestController
