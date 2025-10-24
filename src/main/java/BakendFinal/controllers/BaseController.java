4ce6c3ae5c1ce79144443d8c582b00a98342ca23
3zequiel3 <3zegonzalez@gmail.com>
2025-10-24 14:09:50 +0000

    Cors

--- src/main/java/BakendFinal/controllers/BaseController.java
+++ src/main/java/BakendFinal/controllers/BaseController.java
@@ -3,11 +3,18 @@ package BakendFinal.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import BakendFinal.services.BaseService;
 
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 public abstract class BaseController<D, DC, DE, ID, S extends BaseService<D, DC, DE, ID>> {
     @Autowired
     protected S baseService;
